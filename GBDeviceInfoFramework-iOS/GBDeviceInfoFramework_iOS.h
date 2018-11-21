b3a46dc5925aff781fd16dc47c945e8d942726e6
Santo Gioia <gioia@kapanu.com>
2018-11-21 13:54:18 +0100

    add static framework for ios

---
+++ GBDeviceInfoFramework-iOS/GBDeviceInfoFramework_iOS.h
@@ -0,0 +1,19 @@
+//
+//  GBDeviceInfoFramework_iOS.h
+//  GBDeviceInfoFramework-iOS
+//
+//  Created by Santo Gioia on 21.11.18.
+//  Copyright © 2018 Luka Mirosevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GBDeviceInfoFramework_iOS.
+FOUNDATION_EXPORT double GBDeviceInfoFramework_iOSVersionNumber;
+
+//! Project version string for GBDeviceInfoFramework_iOS.
+FOUNDATION_EXPORT const unsigned char GBDeviceInfoFramework_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GBDeviceInfoFramework_iOS/PublicHeader.h>
+
+
