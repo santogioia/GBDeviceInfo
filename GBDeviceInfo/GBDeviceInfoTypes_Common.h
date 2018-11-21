4585634965d7c5c6bd93f24a35f82ccd28dc752f
Santo Gioia <gioia@kapanu.com>
2018-11-21 14:26:23 +0100

    change settings in Info.plist

--- GBDeviceInfo/GBDeviceInfoTypes_Common.h
+++ GBDeviceInfo/GBDeviceInfoTypes_Common.h
@@ -17,7 +17,6 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-
 typedef NS_ENUM(NSInteger, GBByteOrder) {
     GBByteOrderLittleEndian,
     GBByteOrderBigEndian,
